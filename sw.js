01b1ab0ddac905b36c050f0cd28b2165dfc75250
AOvidiu81 <anitoiuovidiu@gmail.com>
2026-08-31 21:19:01 +0300

    v36: fix WhatsApp/Trimite share timing (root files: manifest.json, sw.js)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // offline completa dupa prima incarcare. La fiecare modificare a
 // aplicatiei, creste CACHE_VERSION ca telefoanele sa preia noua versiune.
 
-const CACHE_VERSION = 'pv-euro-ecologic-v35';
+const CACHE_VERSION = 'pv-euro-ecologic-v36';
 const APP_SHELL = [
   './',
   'index.html',
